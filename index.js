cb847851be8cf8697ce91bc6d6a5cb66a4a13d23
ngrwisnu <adiprayogongrwisnu@gmail.com>
2022-06-22 15:04:22 +0700

    Added funtion to the buttons

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const data = [];
+const books = [];
 const RENDER_BOOK = "render-book";
 
 const formInput = document.getElementById("form");
@@ -12,7 +12,7 @@ formInput.addEventListener("submit", (e) => {
   const author = document.getElementById("author").value;
   const year = document.getElementById("year").value;
 
-  const checkBox = document.getElementById("checkbox").checked;
+  const isRead = document.getElementById("checkbox").checked;
 
   const bookID = +new Date();
 
@@ -21,19 +21,179 @@ formInput.addEventListener("submit", (e) => {
     author,
     year,
     bookID,
-    checkBox,
+    isRead,
   };
 
-  data.push(dataObject);
-  console.log(data);
-
-  data.forEach((val) => {
-    console.log(val);
-  });
+  books.push(dataObject);
 
   document.dispatchEvent(new Event(RENDER_BOOK));
 });
 
+// BOOK CARD
+const bookCard = (bookObject) => {
+  const bookAuthor = document.createElement("span");
+  bookAuthor.innerText = bookObject.author;
+  bookAuthor.classList.add("card-author");
+
+  const bookYear = document.createElement("span");
+  bookYear.innerText = bookObject.year;
+  bookYear.classList.add("card-year");
+
+  const bookTitle = document.createElement("p");
+  bookTitle.innerText = bookObject.title;
+  bookTitle.classList.add("card-title");
+
+  const cardText = document.createElement("div");
+  cardText.classList.add("card-text");
+  cardText.append(bookAuthor, bookYear, bookTitle);
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
+  cardContainer.setAttribute("id", `${bookObject.bookID}`);
+  cardContainer.append(cardText);
+
+  // card buttons setting
+  if (bookObject.isRead) {
+    const undoButton = undoButtonComponent();
+
+    undoButton.addEventListener("click", () => {
+      undoButtonHandler(bookObject.bookID);
+    });
+
+    const deleteButton = deleteButtonComponent();
+
+    deleteButton.addEventListener("click", () => {
+      deleteButtonHandler(bookObject.bookID);
+    });
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("card-buttons");
+    buttonContainer.append(undoButton, deleteButton);
+
+    cardContainer.append(buttonContainer);
+  } else {
+    const doneButton = doneButtonComponent();
+
+    doneButton.addEventListener("click", () => {
+      doneButtonHandler(bookObject.bookID);
+    });
+
+    const deleteButton = deleteButtonComponent();
+
+    deleteButton.addEventListener("click", () => {
+      deleteButtonHandler(bookObject.bookID);
+    });
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("card-buttons");
+    buttonContainer.append(doneButton, deleteButton);
+
+    cardContainer.append(buttonContainer);
+  }
+
+  return cardContainer;
+};
+
+// Button Component Handler
+function doneButtonHandler(bookId) {
+  const bookTarget = findBook(bookId);
+
+  if (bookTarget == null) {
+    return;
+  } else {
+    bookTarget.isRead = true;
+  }
+
+  document.dispatchEvent(new Event(RENDER_BOOK));
+}
+
+function undoButtonHandler(bookId) {
+  const bookTarget = findBook(bookId);
+
+  if (bookTarget == null) {
+    return;
+  } else {
+    bookTarget.isRead = false;
+  }
+
+  document.dispatchEvent(new Event(RENDER_BOOK));
+}
+
+function deleteButtonHandler(bookId) {
+  const bookTarget = findBookIndex(bookId);
+
+  if (bookTarget === null) {
+    return;
+  } else {
+    books.splice(bookTarget, 1);
+  }
+
+  document.dispatchEvent(new Event(RENDER_BOOK));
+}
+// ----------------------------------
+
+function findBook(bookId) {
+  for (const book of books) {
+    if (book.bookID === bookId) {
+      return book;
+    }
+  }
+
+  return null;
+}
+
+function findBookIndex(bookId) {
+  for (const index in books) {
+    if (books[index].bookID === bookId) {
+      return index;
+    }
+  }
+
+  return null;
+}
+
+// Buttons Component
+function undoButtonComponent() {
+  const undoButton = document.createElement("button");
+  undoButton.innerText = "Baca lagi";
+  undoButton.classList.add("card-btn-undo");
+
+  return undoButton;
+}
+
+function doneButtonComponent() {
+  const doneButton = document.createElement("button");
+  doneButton.innerText = "Selesai";
+  doneButton.classList.add("card-btn-selesai");
+
+  return doneButton;
+}
+
+function deleteButtonComponent() {
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Hapus";
+  deleteButton.classList.add("card-btn-hapus");
+
+  return deleteButton;
+}
+// ----------------------------------
+
+// RENDER BOOK CARD
 document.addEventListener(RENDER_BOOK, () => {
-  console.log("Data berhasil di render");
+  const unreadBook = document.getElementById("unread");
+  unreadBook.innerHTML = "";
+
+  const readBook = document.getElementById("read");
+  readBook.innerHTML = "";
+
+  for (const book of books) {
+    const bookElement = bookCard(book);
+
+    if (!book.isRead) {
+      unreadBook.append(bookElement);
+    } else {
+      readBook.append(bookElement);
+    }
+  }
 });
+// ----------------------------------
